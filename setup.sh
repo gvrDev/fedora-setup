32082b62120e0b6a65920eec05fcbed7fda98274
gvrDev <guilhermevrodrigues123@gmail.com>
2026-09-15 00:49:19 -0300

    update

--- setup.sh
+++ setup.sh
@@ -140,7 +140,7 @@ git config --global commit.gpgsign true
 log "Authenticating GitHub"
 sudo dnf install -y gh
 if ! gh auth status &>/dev/null; then
-    gh auth login --web --skip-ssh-key --git-protocol ssh --hostname github.com
+    gh auth login --web --skip-ssh-key --git-protocol ssh --hostname github.com --clipboard
 fi
 
 log "Uploading GitHub SSH key"
