ab77e8f82b7e808373d446bb560e5206232b244c
gvrDev <guilhermevrodrigues123@gmail.com>
2026-09-14 21:10:22 -0300

    update

--- setup.sh
+++ setup.sh
@@ -77,7 +77,7 @@ sudo dnf install -y \
     shellcheck 
 
 log "Installing development tools"
-sudo dnf install -y @development-tools
+sudo dnf group install -y development-tools c-development
 
 log "Generating ssh keys"
 FEDORA_SCRIPT_GITHUB_USER="${FEDORA_SCRIPT_GITHUB_USER:-${FEDORA_SCRIPT_SSH_USERNAME:-}}"
